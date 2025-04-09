0d05fc408d36550fad6ca08ae5840c5f79e43226
amikaawale <awaley03@gmail.com>
2025-04-09 09:52:25 +0545

    fixes

--- DependencyInjection/ZOSupervisorMonitorExtension.php
+++ DependencyInjection/ZOSupervisorMonitorExtension.php
@@ -26,6 +26,9 @@ class ZOSupervisorMonitorExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+
         // set config variable to parameters
         $container->setParameter('zo_supervisor_monitor.servers', $config['servers']);
     }
